063afffb7c0b4bad4936db1e1369861b185f466c
galtiparmak <115663609+galtiparmak@users.noreply.github.com>
2024-11-27 12:37:34 +0300

    Updates

--- src/main/java/com/codeway/devopscasestudy/DevopscasestudyApplication.java
+++ src/main/java/com/codeway/devopscasestudy/DevopscasestudyApplication.java
@@ -2,12 +2,21 @@ package com.codeway.devopscasestudy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
 public class DevopscasestudyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevopscasestudyApplication.class, args);
+	}
+
+	@EventListener
+	public void logProperties(ApplicationReadyEvent event) {
+		ConfigurableEnvironment env = event.getApplicationContext().getEnvironment();
+		System.out.println("spring.datasource.url: " + env.getProperty("spring.datasource.url"));
 	}
 
 }
